6b78d3101a7237cd88b15c0016e79a31e7e559e5
N-Storm <N-Storm@users.noreply.github.com>
2024-05-14 04:19:39 +0300

    Added calculated OCR values for other cases

--- firmware/lib/Livolo/Livolo.h
+++ firmware/lib/Livolo/Livolo.h
@@ -9,21 +9,41 @@
 
 #include "Arduino.h"
 #include <stdint.h>
-#include <UserTimer.h>
 
-#if defined(TIMER_TO_USE_FOR_USER) && TIMER_TO_USE_FOR_USER == 1
-#define DL_TIMER 1
-#define OCR_HALFBIT  82
-#define OCR_FULLBIT 164
-#define OCR_START   255
+#define DL_TIMER_PLL 11
+
+#if defined(__AVR_ATtinyX5__) && defined(TIMER_TO_USE_FOR_MILLIS)
+  #include <UserTimer.h>
+  #if defined(TIMER_TO_USE_FOR_USER) && TIMER_TO_USE_FOR_USER == 1
+    #define DL_TIMER DL_TIMER_PLL
+    #define OCR_HALFBIT  82
+    #define OCR_FULLBIT 164
+    #define OCR_START   255
+  #else
+    #if defined(TIMER_TO_USE_FOR_USER) && TIMER_TO_USE_FOR_USER == 0
+      #define DL_TIMER 0
+      #error "Timer 0 on DigiStump core are not supported yet. Please change TIMER_TO_USE_FOR_MILLIS to 0 in Digistump core to make Timer 1 available."
+    #else
+      #error "Unknown ATTiny85 core or unknown configuration."
+    #endif
+  #endif
 #else
-#define DL_TIMER 0
-#define OCR_HALFBIT  42
-#define OCR_FULLBIT  84
-#define OCR_START   137
+  #define DL_TIMER 1
 #endif
 
-#if __GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 7)
+#if DL_TIMER != DL_TIMER_PLL
+  #define OCR_START_US 530ULL
+  #define OCR_BIT_US 320ULL
+  #define DL_TIMER_PRESCALER 64
+
+  #define OCR_START_I ((OCR_START_US * F_CPU + (1000000 * DL_TIMER_PRESCALER / 2)) / (1000000 * DL_TIMER_PRESCALER))
+  #define OCR_START OCR_START_I <= 255 ? OCR_START_I : 255
+  #define OCR_FULLBIT_I ((OCR_BIT_US * F_CPU + (1000000 * DL_TIMER_PRESCALER / 2)) / (1000000 * DL_TIMER_PRESCALER))
+  #define OCR_FULLBIT OCR_FULLBIT_I % 2 == 1 ? (OCR_FULLBIT_I+1) : (OCR_FULLBIT_I)
+  #define OCR_HALFBIT (OCR_FULLBIT)/2
+#endif
+
+#if defined(__AVR__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 7))
 typedef union {
   __uint24 buf;
   uint8_t bytes[3];
